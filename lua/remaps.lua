210be74ffcd52a53e7b8cab8e55511d54cc13b7b
Lufflee-Vaflee <garriharripuff@gmail.com>
2025-04-06 22:34:26 +0300

    quick-fix remaps

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -3,6 +3,8 @@ print("Hello from remap")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>u", ":u<cr>")
-vim.keymap.set("n", "<leader>w", "<C-W>")
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<M-q>", "<cmd>cclose<CR>")
 
