b736087592b111baf8fa37a8b913076636c1fd68
Lufflee-Vaflee <garriharripuff@gmail.com>
2025-04-06 19:26:48 +0300

    enchance, git tools

--- lua/gitTools.lua
+++ lua/gitTools.lua
@@ -51,39 +51,9 @@ gitgraph.setup({
     }
 })
 
-local inspect_output = vim.api.nvim_exec2("Inspect", {output = true}).output
-
-print(inspect_output)
-
-function attach_to_buffer(buffer_id)
-    buffer_id = buffer_id or 0  -- Default to current buffer if none specified
-
-    -- Create an autocommand group for our cursor events
-    local cursor_group = vim.api.nvim_create_augroup("CursorMovementGroup_" .. buffer_id, { clear = true })
-
-    -- Normal mode cursor movement
-    vim.api.nvim_create_autocmd("CursorMoved", {
-        group = cursor_group,
-        buffer = buffer_id,
-        callback = function()
-            local cursor = vim.api.nvim_win_get_cursor(0)
-            print("Normal mode - Line: " .. cursor[1] .. ", Column: " .. cursor[2])
-
-            -- Your custom logic here when cursor moves in normal mode
-        end,
-    })
-
-    print("Cursor movement detection attached to buffer " .. buffer_id)
-
-    local inspect_output = vim.api.nvim_exec2("Inspect", {output = true}).output
-
-    
-end
-
 local on_gitgraph_open = function()
     vim.cmd.NvimTreeClose()
     gitgraph.draw({}, { all = true, max_count = 5000 })
-    attach_to_buffer(0)
 end
 
 -- Keymappings for gitgraph.nvim
